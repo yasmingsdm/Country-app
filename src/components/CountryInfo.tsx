e26a6abed7e42e651c500178aa3ff27eccbf6eac
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-03-21 12:01:08 +0100

    css

--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -3,7 +3,7 @@
 const CountryInfo = ({data})=>{
     const {name, flag, population, continents, languages, capital } = data.length && data[0];
 console.log(languages)
-// const speak = languages.map(language =>
+// const speak = Object.values(languages).map(language =>
 //     <ul><li>{language}</li></ul>)
 // console.log(speak)
     return (
@@ -11,10 +11,8 @@ console.log(languages)
         {data.length && <article>
         <h1>{name.common}</h1>
         <h1 className="flag">{flag}</h1>
-        <p>
-            {name.common} is located in {continents} and has a population of {population} people. They
-            speak {} and their capital is {capital}.
-        </p>
+        <h2> {name.common} is located in {continents} and has a population of {population} people. </h2>
+        <h2> They speak {} and their capital is {capital}.</h2>
         </article>}
         </>)
 }
