ae42d8e07cde2a6d7a18b62ee6d438af6895a85d
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-03-21 09:08:04 +0100

    testing almost ready

--- src/components/Testing.tsx
+++ src/components/Testing.tsx
@@ -1,22 +1,35 @@
 //@ts-nocheck
 
 import { useState } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Testing = (data)=>{
     const [countryName, setCountryName] = useState('')
+    const [index, setIndex] = useState(0)
 
-    const handleChange = ()=>{
-        
+    const handleChange = (e)=>{
+        setCountryName(e.target.value)
     }
+
     const handleSubmit = (e)=>{
         e.preventDefault()
-
+        if(countryName.toLowerCase() === data.data[index].name.common.toLowerCase()){
+            toast('Great! You did it right!')
+            setIndex(prevIndex => prevIndex + 1)
+            setCountryName('')
+        }else{
+            toast('Oh, no! Maybe you need to study a little bit more!')
+        }
+        
     }
+
     return (
         <section>
-            {data.data.length > 0 && <h1 className="flag">{data.data[0].flag} </h1>}
+            {data.data.length > 0 && <h1 className="flag">{data.data[index].flag} </h1>}
             <input type='text' placeholder="Type the name of the country" onChange={handleChange}/>
-            <button onSubmit={handleSubmit}>Submit</button>
+            <button type='submit' onClick={handleSubmit}>Submit</button>
+            <ToastContainer/>
         </section>
        
     )}
