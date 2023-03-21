aed765c3deced04740162dba439de3896e61a454
Yasmin Dal Medico <yasmin.dm@gmail.com>
2023-03-21 13:51:02 +0100

    favorite and country details

--- src/Pages/Favorite.tsx
+++ src/Pages/Favorite.tsx
@@ -1,11 +1,35 @@
+//@ts-nocheck
+
+import { FaAngleRight } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+
 const Favorite = ()=>{
     const favouriteList = localStorage.getItem('favoriteCountry')
-    const result = favouriteList ? JSON.parse(favouriteList): undefined
-    console.log(result)
+    const result = favouriteList ? JSON.parse(favouriteList): []
 
-    // if(name)return, 
+    const favoriteCountries = result.map(country=>(  
+        <tr key={Math.random()}>
+            <td>{country.name.common}</td>
+            <td className="table__flag">{country.flag}</td>
+            <td>{country.continents}</td>
+            <td><Link to={'/' + country.name.common}><FaAngleRight/></Link></td>
+        </tr>))
+    
     return (
-        <h1>fav</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Country</th>
+                    <th>Flag</th>
+                    <th>Continent</th>
+                    <th>More info</th>
+                </tr>
+            </thead>
+            <tbody>
+                {favoriteCountries}
+            </tbody>
+        </table>
     )}
 
     export default Favorite
--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -2,17 +2,19 @@
 
 const CountryInfo = ({data})=>{
     const {name, flag, population, continents, languages, capital } = data.length && data[0];
-console.log(languages)
-// const speak = Object.values(languages).map(language =>
-//     <ul><li>{language}</li></ul>)
-// console.log(speak)
+
     return (
         <>
         {data.length && <article>
         <h1>{name.common}</h1>
         <h1 className="flag">{flag}</h1>
-        <h2> {name.common} is located in {continents} and has a population of {population} people. </h2>
-        <h2> They speak {} and their capital is {capital}.</h2>
+        <div className="infoCountry">
+        <h2>Capital: {capital} </h2>
+        <h2>Continent: {continents} </h2>
+        <h2>Population: {population} people</h2>
+        </div>
+        <h2> People there speaks: {languages && <ul> {(Object.values(languages)).map(language =>
+    <li key={Math.random()}>{language}</li>)}</ul>}</h2>
         </article>}
         </>)
 }
